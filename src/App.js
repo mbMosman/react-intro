0e1262eb929f262a0f06d4ba1e158ba37d01f61c
mbmosman <mbMosman@users.noreply.github.com>
2018-03-14 10:58:54 -0500

    moved header to component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'; // Uses destructuring
+// Could also do:
+//const Component = React.Component;
+// Below could also use React.Component instead of Component
+
 import './App.css';
+import Header from './Header';
 
 class App extends Component {
   render() {
+    // Uses JSX - JavaScript XML, react interprets this behind the scenes
+    // Allows us to use HTML as if it is part of JavaScript
+    // Behind the scenes it is doing things like:
+    // const div = React.createElement("div", null, "React Intro");
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <Header />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// Uses JSX - JavaScript XML
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+
